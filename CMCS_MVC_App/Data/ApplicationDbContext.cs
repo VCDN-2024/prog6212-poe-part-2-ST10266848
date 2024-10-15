168ae8635add9e2e69174ceffce7b947eb5ac8dd
ST10266848 <ST10266848@vcconnect.edu.za>
2024-10-15 10:52:41 +0200

    Second Commit
    - Deleted the project called 'CMCS_MVC' in favour of creating a MVC project called 'CMCS_MVC_App' as it is more descriptive.
    - Also, I implemented Microsoft AspNetCore Identities and role authentication, thanks to the help of Digital Tech Joint's Videos on YouTube:
       - There is currently a register and
          login system as well as the
          implementation of different users
         with certain roles.

---
+++ CMCS_MVC_App/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMCS_MVC_App.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        
+    }
+}
